14198d62fa5bf2bde0119040314b080ec5919163
ee7klt <kltah1@gmail.com>
2014-07-13 09:30:40 +0800

    overload toString()

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,84 @@
 
+/**
+ * Implementation of a card type.
+ * 
+ * @author Alex Tah
+ * @since July 13 2014
+ */
+
+
 public class Card {
+	private Suit mySuit;      //one of the four valid suits
+	private int myNumber;     //value of card. Ace:1, J-K: 11-13
+	
+	
+	
+	/**
+	 * Card constructor
+	 * @param aSuit      suit of the card
+	 * @param aNumber    number of the card
+	 */
+	
+	public Card(Suit aSuit, int aNumber) {
+		this.mySuit = aSuit;
+		this.myNumber = aNumber;
+	}
+	
+	/**
+	 * Return number of the card.
+	 * @return the number
+	 */
+	public int getNumber() {
+		return myNumber;
+	}
+	
+	public String toString() {   //overload object class method, make it our own
+		String numStr = "Error";
+		
+		switch(this.myNumber) {
+		
+		case 2:
+			numStr = "Two";
+			break;
+		case 3:
+			numStr = "Three";
+			break;
+		case 4:
+			numStr = "Four";
+			break;
+		case 5:
+			numStr = "Five";
+			break;
+		case 6:
+			numStr = "Six";
+			break;
+		case 7:
+			numStr = "Seven";
+			break;
+		case 8:
+			numStr = "Eight";
+			break;
+		case 9:
+			numStr = "Nine";
+			break;
+		case 10:
+			numStr = "Ten";
+			break;
+		case 11:
+			numStr = "Jack";
+			break;
+		case 12:
+			numStr = "Queen";
+			break;
+		case 13:
+			numStr = "King";
+			break;
+		case 1:
+			numStr = "Ace";
+			break;
+		}
+		
+		return numStr + "of" + mySuit.toString();
+	}
 
 }
--- src/Suit.java
+++ src/Suit.java
@@ -1,4 +1,18 @@
+/**
+ * Four possible suits of cards
+ * 
+ * @author Alex Tah
+ * @since July 13 2014
+ *
+ */
+
+
+
+
 
 public enum Suit {
-
+	Clubs,
+	Diamonds,
+	Spades,
+	Hearts
 }
